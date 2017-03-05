b238ecf506b3c4695b61fa38c501ae04feaf8bbc
michal pisanko <mpisanko@gmail.com>
2017-03-05 21:58:02 +1100

    extract functions

--- lib/survey.ex
+++ lib/survey.ex
@@ -9,15 +9,23 @@ defmodule Survey do
   end
 
   defp validate!({flags, _, _}) do
+      flags |> validate_flags! |> validate_files!
+  end
+
+  defp validate_flags!(flags) do
     if (flags |> Keyword.keys |> Enum.sort == @flags |> Keyword.keys |> Enum.sort) do
-      if (flags |> Keyword.values |> Enum.all?(&match?({:ok,_}, File.stat(&1)))) do
-        flags
-      else
-        raise ArgumentError, ~s[File(s) do not exist: #{flags|>Keyword.values|>Enum.reject(&match?({:ok,_},File.stat(&1)))}]
-      end
       flags
     else
       raise ArgumentError, "Please specify --survey and --response with paths pointing to respective files."
     end
+  end
+
+  defp validate_files!(flags) do
+    if (flags |> Keyword.values |> Enum.all?(&match?({:ok,_}, File.stat(&1)))) do
+      flags
+    else
+      raise ArgumentError, ~s[File(s) do not exist: #{flags|>Keyword.values|>Enum.reject(&match?({:ok,_},File.stat(&1)))}]
+    end
+    
   end
 end
