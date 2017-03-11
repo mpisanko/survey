3500dd606d19ddd0082643308341595bee1d7bf0
michal pisanko <mpisanko@gmail.com>
2017-03-11 21:14:53 +1100

    use CLI processor and call calculators to process results. format a string to present in console output

--- lib/survey.ex
+++ lib/survey.ex
@@ -1,12 +1,28 @@
 defmodule Survey do
-  alias Survey.Providers.Cli
 
   defstruct survey: [], response: []
 
   def main(args \\ []) do
     args
-    |> Cli.data
-    |> IO.inspect
+    |> Survey.Providers.Cli.data
+    |> calculate_results
+    |> present_results
+    |> IO.puts
   end
 
+  defp calculate_results(survey) do
+    [Survey.Calculators.Participation, Survey.Calculators.AverageRating]
+    |> Enum.map(fn c -> c.calculate(survey) end)
+    |> Enum.reduce(%{}, fn (%{type: t, result: r}, acc) -> Map.put(acc, t, r) end)
+  end
+
+  defp present_results(%{participation: p, average_rating: ar}) do
+    ["Participation in survey was: #{Float.to_string(p.percent, decimals: 2)}% out of #{p.total}\nRating\t|Theme\t\t|Question"] ++
+    (ar |> Enum.map(&present_question/1))
+    |> Enum.join("\n")
+  end
+
+  defp present_question(%{text: text, theme: theme, average: average}) do
+    "#{average}\t|#{theme}\t|#{text}"
+  end
 end
