4f6147c2f011050e08a4aead36c436bf6a1a5149
michal pisanko <mpisanko@gmail.com>
2017-03-11 15:40:42 +1100

    extract submitted into Calculator

--- lib/survey/calcuators/calculator.ex
+++ lib/survey/calcuators/calculator.ex
@@ -1,3 +1,7 @@
-defmodule Survey.Caluculators.Calculator do
+defmodule Survey.Calculators.Calculator do
   @callback calculate(s :: %Survey{survey: [%{}], response: [%{}]}) :: %{type: String.t, result: any()}
+
+  def submitted(rs) do
+    rs |> Enum.filter(fn e -> e[:submitted_at] != nil end)
+  end
 end
